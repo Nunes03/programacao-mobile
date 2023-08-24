bbb56a41ac9f5496ad8b0a2774b6bf14d5ddae81
Lucas Nunes <69318819+Nunes03@users.noreply.github.com>
2023-08-24 19:04:37 -0300

    Add files via upload

---
+++ app/src/main/java/io/github/nunes03/pet_shop/HomeActivity.kt
@@ -0,0 +1,11 @@
+package io.github.nunes03.pet_shop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
